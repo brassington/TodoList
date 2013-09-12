458a9b19aa12869dfad08935fbb06113e5ac2e48
J. Andrew Brassington <jabbrass@zoho.com>
2013-09-12 14:44:56 -0500

    Added Adapters and Mock Adapter

---
+++ TodoList/Adapters/Mock/ToDoListMockAdapter.cs
@@ -0,0 +1,20 @@
+﻿using TodoList.Data.Model;
+using TodoList.Adapters.Interfaces;
+using TodoList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoList.Adapters.Mock
+{
+    public class ToDoListMockAdapter : IToDoListAdapter
+    {
+        public ToDoListViewModel GetToDoListViewModel()
+        {
+            ToDoListViewModel model = 
+        }
+        
+        
+        }
+}
---
+++ TodoList/Models/ToDoListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TodoList.Data.Model;
+
+namespace TodoList.Adapters.Interfaces
+{
+    public class ToDoListViewModel
+    {
+        public int TaskId { get; set; }
+        public Task Task { get; set; }
+    }
+}
