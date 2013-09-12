c1594490dab099344fb69a57e82ed9462a6fda91
J. Andrew Brassington <jabbrass@zoho.com>
2013-09-12 13:59:38 -0500

    Created Classes

---
+++ TodoList.Data.Model/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Data.Model
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public virtual List<Task> Tasks { get; set; }
+    }
+}
---
+++ TodoList.Data.Model/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Data.Model
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime TaskBegin { get; set; }
+        public DateTime TaskEnd { get; set; }
+
+        public virtual List<Category> Categories { get; set; }
+    }
+}
